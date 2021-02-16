65e464db49ec6ddab22bce6e9c2447fa873a8bf1
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-16 12:31:01 +0700

    Delete test no longer needed
    We now cancel reservation, not order

--- tests/Unit/OrderModelTest.php
+++ tests/Unit/OrderModelTest.php
@@ -23,21 +23,6 @@ class OrderModelTest extends TestCase
         ]);
     }
 
-    public function test_order_can_be_canceled()
-    {
-        // Arrange
-        $concert = Concert::factory()->published()->create()->addTickets(10);
-        $order = $concert->orderTickets('john@gmail.com', 5);
-        $this->assertEquals($concert->ticketsRemaining(), 5);
-
-        // Act
-        $order->cancel();
-
-        // Assert
-        $this->assertEquals($concert->ticketsRemaining(), 10);
-        $this->assertNull(Order::find($order->id));
-    }
-
     public function test_creating_order_from_tickets_and_email_and_amount()
     {
         // Arrange
