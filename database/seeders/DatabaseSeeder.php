3f94c910b5e862b3ffc97b729bb23dc21b8f5276
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-17 12:47:08 +0700

    Seed db with 1 concert and 5 tickets

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Concert;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Concert::factory()->published()->create([
+            'title' => 'Example Title',
+            'subtitle' => 'with some fake subTitles',
+            'date' => Carbon::parse('+2 weeks'),
+            'ticket_price' => 1250,
+            'venue' => 'The House',
+            'venue_address' => 'Some mysterious street',
+            'city' => 'Hanoi',
+            'state' => 'North',
+            'zip' => '20056',
+            'additional_information' => 'Feel free to contact us by email: example@gmail.com'
+        ])->addTickets(5);
     }
 }
