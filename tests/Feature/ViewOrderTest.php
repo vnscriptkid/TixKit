1879350609f6b33a57cbb03978587b967b963695
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-20 09:54:34 +0700

    Add more assertions to viewOrderTest

--- tests/Feature/ViewOrderTest.php
+++ tests/Feature/ViewOrderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Concert;
 use App\Models\Order;
 use App\Models\Ticket;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -16,18 +17,29 @@ class ViewOrderTest extends TestCase
     {
         $this->withoutExceptionHandling();
         // Arrange
-        $concert = Concert::factory()->create();
+        $concert = Concert::factory()->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('March 12, 2017 8:00pm'),
+            'ticket_price' => 4250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+        ]);
         $order = Order::factory()->create([
             'confirmation_number' => 'CONFIRMATIONNUMBER123',
             'amount' => 4550,
-            'card_last_four' => '4242'
+            'card_last_four' => '4242',
+            'email' => 'john@example.com',
         ]);
-        Ticket::factory()->create([
+        $ticketA = Ticket::factory()->create([
             'concert_id' => $concert->id,
             'order_id' => $order->id,
             'code' => 'TICKETCODE123'
         ]);
-        Ticket::factory()->create([
+        $ticketB = Ticket::factory()->create([
             'concert_id' => $concert->id,
             'order_id' => $order->id,
             'code' => 'TICKETCODE456'
@@ -46,5 +58,13 @@ class ViewOrderTest extends TestCase
         $response->assertSee('**** **** **** 4242');
         $response->assertSee('TICKETCODE123');
         $response->assertSee('TICKETCODE456');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON');
+        $response->assertSee('17916');
+        $response->assertSee('john@example.com');
+        $response->assertSee('2017-03-12 20:00');
     }
 }
