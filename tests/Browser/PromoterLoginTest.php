2eb4c85267501c68e9e52f98234909c555af6e2a
Thanh Nguyen <vnscriptkid@gmail.com>
2021-02-24 18:33:15 +0700

    Update dusk tests to use DatabaseMigrations instead

--- tests/Browser/PromoterLoginTest.php
+++ tests/Browser/PromoterLoginTest.php
@@ -3,27 +3,27 @@
 namespace Tests\Browser;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class PromoterLoginTest extends DuskTestCase
 {
-    use RefreshDatabase;
+    use DatabaseMigrations;
 
     public function test_logging_in_successfully()
     {
         User::factory()->create([
-            'email' => 'thanh@gmail.com',
-            'password' => bcrypt('12345678')
+            'email' => 'taylor@laravel.com',
+            'password' => bcrypt('HardToGuess')
         ]);
 
         $this->browse(function (Browser $browser) {
             $browser->visit('/login')
-                ->type('email', 'thanh@gmail.com')
-                ->type('password', '12345678')
+                ->type('email', 'taylor@laravel.com')
+                ->type('password', 'HardToGuess')
                 ->press('Log in')
-                ->assertPathIs('/backstage/concerts');
+                ->assertPathIs('/backstage/concerts/new');
         });
     }
 
