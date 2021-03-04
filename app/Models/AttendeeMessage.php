774f7e96c80cb572a3bc672849f8a10d04af3bdf
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-05 01:23:33 +0700

    Refactor to fetch and send emails by chunks
    Avoid polutting emails

--- app/Models/AttendeeMessage.php
+++ app/Models/AttendeeMessage.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Mail\AttendeeMessageEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
 
 class AttendeeMessage extends Model
 {
@@ -14,5 +16,17 @@ class AttendeeMessage extends Model
     public function concert()
     {
         return $this->belongsTo(Concert::class);
+    }
+
+    public function orders()
+    {
+        return $this->concert->orders();
+    }
+
+    public function withChunkedRecipients($chunkSize, $callback)
+    {
+        $this->orders()->chunk($chunkSize, function ($orders) use ($callback) {
+            $callback->__invoke($orders->pluck('email'));
+        });
     }
 }
