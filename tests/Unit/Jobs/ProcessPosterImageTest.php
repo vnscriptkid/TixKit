1834b32e444ed88defc63a681ac2374a75a57fcf
Thanh Nguyen <vnscriptkid@gmail.com>
2021-03-06 17:37:33 +0700

    Optimize image size by limiting colors

--- tests/Unit/Jobs/ProcessPosterImageTest.php
+++ tests/Unit/Jobs/ProcessPosterImageTest.php
@@ -33,4 +33,25 @@ class ProcessPosterImageTest extends TestCase
         $this->assertEquals(600, $width);
         $this->assertEquals(776, $height);
     }
+
+    public function test_it_optimizes_the_image()
+    {
+        // Arrange
+        Storage::fake('public');
+        Storage::disk('public')->put(
+            'posters/example-poster.png',
+            file_get_contents(base_path('tests/__fixtures__/small-unoptimized-poster.png'))
+        );
+        $concert = ConcertFactory::createUnpublished([
+            'poster_image_path' => 'posters/example-poster.png'
+        ]);
+
+        // Act
+        ProcessPosterImage::dispatch($concert);
+
+        // Assert
+        $originalFileSize = filesize(base_path('tests/__fixtures__/small-unoptimized-poster.png'));
+        $optimizedFileSize = Storage::disk('public')->size('posters/example-poster.png');
+        $this->assertTrue($optimizedFileSize < $originalFileSize);
+    }
 }
